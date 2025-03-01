63219fcb985b50b2104f5983e52541f9baebb49b
Chang Wei <changwei1006@gmail.com>
2025-03-02 02:17:17 +0800

    update HomeHero: add style

--- src/app/(home)/HomeHero/HomeHero.tsx
+++ src/app/(home)/HomeHero/HomeHero.tsx
@@ -3,7 +3,7 @@ import image_hero from "./hero.png";
 
 export default function HomeHero() {
   return (
-    <div className="">
+    <div className="max-w-[1440px] mx-auto flex justify-between items-center">
       <Image src={image_hero.src} alt="" width={1440} height={1024} />
     </div>
   );
