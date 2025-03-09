73e5d1e489b20f1496a0c72f1d1d722411121e53
Riley Ho <riley@rileychh.dev>
2025-03-09 15:34:33 +0800

    fix: use Noto Sans TC webfont instead of Roboto & local CJK fonts
    Co-authored-by: pan93412 <me@pan93.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Noto_Sans_TC } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer/Footer";
 
-const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "700"],
+const notoSansTC = Noto_Sans_TC({
+  weight: "variable",
 });
 
 export const metadata: Metadata = {
@@ -22,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} antialiased`}>
+      <body className={`${notoSansTC.className} antialiased`}>
         <Header />
         <main>{children}</main>
         <Footer />
