2397e97ba5c49117e7c95d0e44a902b458b86d83
Chang Wei <changwei1006@gmail.com>
2025-03-16 22:43:07 +0800

    update: Header, add scrollspy

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,26 @@
+"use client"
+
 import HomeHero from "@/app/(home)/HomeHero/HomeHero";
 import HomeIntroduction from "@/app/(home)/HomeIntroduction/HomeIntroduction";
 import HomeInformation from "./HomeInformation/HomeInformation";
 import HomeSignUp from "@/app/(home)/HomeSignUp/HomeSignUp";
 import HomeSponsor from "@/app/(home)/HomeSponsor/HomeSponsor";
+import ScrollSpy from "react-ui-scrollspy";
 //import HomeNotice from "@/app/(home)/HomeNotice/HomeNotice";
 
 export default function Home() {
   return (
     <div className="bg-[#EDEAE5]">
-      <HomeHero />
-      <HomeIntroduction />
-      <HomeInformation />
-      <HomeSignUp />
-      <HomeSponsor />
+      <ScrollSpy
+        activeClass="border-b-[5px]"
+        scrollThrottle={100}
+      >
+        <HomeHero />
+        <HomeIntroduction />
+        <HomeInformation />
+        <HomeSignUp />
+        <HomeSponsor />
+      </ScrollSpy>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import svg_devjam from "@/assets/devjam.svg";
+import Link from "next/link";
 
 export const Header: React.FC = () => {
   const menus = [
@@ -8,24 +11,39 @@ export const Header: React.FC = () => {
     { label: "活動資訊", value: "home-information", mobileLabel: "資訊" },
     { label: "報名資訊", value: "home-sign-up", mobileLabel: "報名" },
   ];
-  {
-    /* label: "成果展現", value: "home-achievements", mobileLabel: "成果" }*/
-  }
+
+  const onPress = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const target = window.document.getElementById(
+      e.currentTarget.href.split("#")[1]
+    );
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="w-full fixed top-0 bg-white/40 backdrop-blur-lg z-50 overflow-hidden px-4">
       <div className="max-w-full mx-auto flex items-center justify-between">
         <div className="flex items-center">
-          <Image src={svg_devjam.src} alt={"DevJam"} width={158} height={61} />
+          <Link href="/">
+            <Image
+              src={svg_devjam.src}
+              alt="DevJam"
+              width={158}
+              height={61}
+              className="md:w-[158px] md:h-[61px] w-[105px] h-[40px]"
+            />
+          </Link>
         </div>
         <div>
           <ul className="flex space-x-4 list-none justify-between">
             {menus.map((menu, index) => (
               <li
-                key={index}
-                className="border-b-[5px] border-gray-300 m-[0.25rem] border-b-[#4c4c4c]"
+                key={index} data-to-scrollspy-id={menu.value}
+                className="border-b-0 m-[0.25rem] border-b-[#4c4c4c] transition-all duration-300"
               >
-                <a href={`#${menu.value}`}>
+                <a href={`#${menu.value}`} onClick={(e) => onPress(e)}>
                   <span className="hidden md:block text-[1.5rem] p-[1rem]">
                     {menu.label}
                   </span>
