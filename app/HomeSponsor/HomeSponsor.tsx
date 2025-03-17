82d449b037366eaeaca73340c319cb689f5af6ca
Riley Ho <riley@rileychh.dev>
2025-03-18 02:20:41 +0800

    feat: rewrite Sponsor using shared components
    Co-authored-by: pan93412 <me@pan93.com>

--- app/HomeSponsor/HomeSponsor.tsx
+++ app/HomeSponsor/HomeSponsor.tsx
@@ -1,47 +1,39 @@
 import Image from "next/image";
+import H1 from "../components/H1";
+import H2 from "../components/H2";
+import P from "../components/P";
 import image_nccu from "./assets/gdg_on_campus_nccu.svg";
 import image_nthu from "./assets/gdg_on_campus_nthu.svg";
 
 export default function HomeSponsor() {
   return (
-    <div className="text-center pt-20 md:px-10 max-w-[1440px] mx-auto">
-      <h1 className="text-5xl text-title text-center md:text-left md:px-10">
-        贊助單位
-      </h1>
-      <div className="py-2 px-4 md:px-6 lg:px-8 text-left leading-10">
-        <div className="pt-10">
-          <h2 className="text-title text-left">/主辦單位/</h2>
-          <div className="justify-center items-center pt-10 flex flex-col md:flex-row">
-            <Image
-              src={image_nccu.src}
-              alt={"GDG on Campus NCCU"}
-              width={360}
-              height={60}
-            />
-            <Image
-              src={image_nthu.src}
-              alt={"GDG on Campus NTHU"}
-              width={360}
-              height={60}
-            />
-            <Image
-              src={image_nthu.src}
-              alt={"GDG on Campus NTUST"}
-              width={360}
-              height={60}
-            />
-          </div>
-        </div>
-        <div className="pt-10">
-          <h2 className="text-title text-left">/協辦單位/</h2>
-          <div>
-            <p className="text-[18px] leading-[44px] py-10">
-              全台 GDG On Campus（共23間學校）
-            </p>
-          </div>
-        </div>
+    <article id="home-sponsor" className="container mx-auto px-6">
+      <H1>贊助單位</H1>
+
+      <H2>主辦單位</H2>
+      <div className="flex flex-col xl:flex-row items-center justify-center gap-8">
+        <Image
+          src={image_nccu.src}
+          alt="GDG on Campus NCCU"
+          width={360}
+          height={60}
+        />
+        <Image
+          src={image_nthu.src}
+          alt="GDG on Campus NTHU"
+          width={360}
+          height={60}
+        />
+        <Image
+          src={image_nthu.src}
+          alt="GDG on Campus NTUST"
+          width={360}
+          height={60}
+        />
       </div>
-      <hr />
-    </div>
+
+      <H2>協辦單位</H2>
+      <P className="mb-9">全台 GDG On Campus（共23間學校）</P>
+    </article>
   );
 }
