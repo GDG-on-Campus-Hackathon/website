c84aa8428a2f3263d006fb4d66ada2ce1d4481d7
Riley Ho <riley@rileychh.dev>
2025-03-18 02:20:53 +0800

    feat: rewrite Notice using shared components
    Co-authored-by: pan93412 <me@pan93.com>

--- app/HomeNotice/HomeNotice.tsx
+++ app/HomeNotice/HomeNotice.tsx
@@ -1,26 +1,31 @@
+import H2 from "../components/H2";
+
+function ListItem({ children }: { children: React.ReactNode }) {
+  return <li className="text-left pl-1">{children}</li>;
+}
+
 export default function HomeNotice() {
   return (
-    <div id="notice" className="max-w-[1440px] mx-auto py-20 px-5 md:px-10">
-      <h1 className="text-title-1 text-left text-[#4C4C4C] pb-9 pt-3">
-        * 注意事項
-      </h1>
-      <ol className="px-5 md:px-10 list-decimal leading-10">
-        <li className="text-left pl-1">
+    <article id="notice" className="container mx-auto px-6">
+      <H2>注意事項</H2>
+
+      <ol className="space-y-6 list-decimal list-outside ml-6">
+        <ListItem>
           主辦單位得於本活動及其他相關活動中，公開展示所有參賽作品。
-        </li>
-        <li className="text-left pl-1">
+        </ListItem>
+        <ListItem>
           參賽團隊成員資料若查有不實者，主辦單位可隨時取消其競賽資格。參賽團隊成員經提報後若有變更，須送交書面資料經主辦單位審查同意。
-        </li>
-        <li className="text-left pl-1">
+        </ListItem>
+        <ListItem>
           入圍作品如有仿冒抄襲或違反本競賽相關規定等，經查證屬實者，將追回資格與獎勵，並由參賽者自負法律責任，主辦單位不負任何法律責任。
-        </li>
-        <li className="text-left pl-1">
+        </ListItem>
+        <ListItem>
           違反相關規範者，可能導致扣分、取消參賽資格、入圍或得獎資格，並追回其已領取之獎座、獎金及自活動網頁移除其作品。
-        </li>
-        <li className="text-left pl-1">
+        </ListItem>
+        <ListItem>
           主辦單位保留修改本辦法之權利，參加活動者一旦參加本活動，則表示同意接受本辦法之拘束，如有未盡事宜，主辦單位保留各項競賽辦法解釋及變更之權利。
-        </li>
+        </ListItem>
       </ol>
-    </div>
+    </article>
   );
 }
