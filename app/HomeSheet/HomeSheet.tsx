adcb6d4fa8eb18c6fdbba745089a9579c1a82fb4
viiccwen <viiccwen@gmail.com>
2025-03-18 12:41:28 +0800

    refine font and style for improved readability

---
+++ app/HomeSheet/HomeSheet.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import { Sheet, SheetTrigger, SheetContent, SheetTitle } from "./sheet";
+
+interface HomeSheetProps {
+  side: "top" | "bottom" | "left" | "right";
+  isSheetOpen: boolean;
+  setIsSheetOpen: (open: boolean) => void;
+  children: React.ReactNode;
+}
+
+export const HomeSheet: React.FC<HomeSheetProps> = ({
+  side,
+  isSheetOpen,
+  setIsSheetOpen,
+  children,
+}: HomeSheetProps) => {
+  return (
+    <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+      <SheetTrigger>
+        <Menu className="w-6 h-6" />
+      </SheetTrigger>
+      <SheetContent side={side}>
+        <SheetTitle></SheetTitle>
+        {children}
+      </SheetContent>
+    </Sheet>
+  );
+};
