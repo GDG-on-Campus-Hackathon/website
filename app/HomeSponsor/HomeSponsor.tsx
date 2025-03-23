04f25661704ab3c1dd60a1d628847490c8679464
Riley Ho <riley@rileychh.dev>
2025-03-23 15:29:29 +0800

    feat: add TMU IEEC to co-organizers, adjust sponsors layout

--- app/HomeSponsor/HomeSponsor.tsx
+++ app/HomeSponsor/HomeSponsor.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import Article from "../components/Article";
 import H1 from "../components/H1";
 import H2 from "../components/H2";
-import P from "../components/P";
+import SponsorContainer from "./SponsorContainer";
 import image_nccu from "./assets/gdg_on_campus_nccu.svg";
 import image_nthu from "./assets/gdg_on_campus_nthu.svg";
+import image_tmu_ieec from "./assets/tmu_ieec.svg";
 
 export default function HomeSponsor() {
   return (
@@ -12,29 +13,39 @@ export default function HomeSponsor() {
       <H1>贊助單位</H1>
 
       <H2>主辦單位</H2>
-      <div className="flex flex-col items-center justify-center gap-8 mb-9">
+      <SponsorContainer>
         <Image
           src={image_nccu.src}
           alt="GDG on Campus NCCU"
-          width={360}
-          height={60}
+          width={180}
+          height={30}
         />
         <Image
           src={image_nthu.src}
           alt="GDG on Campus NTHU"
-          width={360}
-          height={60}
+          width={180}
+          height={30}
         />
         <Image
           src={image_nthu.src}
           alt="GDG on Campus NTUST"
-          width={360}
-          height={60}
+          width={180}
+          height={30}
         />
-      </div>
+      </SponsorContainer>
 
       <H2>協辦單位</H2>
-      <P>全台 GDG On Campus（共23間學校）</P>
+      <SponsorContainer>
+        <p className="text-sm/relaxed text-justify tracking-wide">
+          全台 GDG On Campus（共26間學校）
+        </p>
+        <Image
+          src={image_tmu_ieec.src}
+          alt="北醫創新創業教育中心"
+          width={184}
+          height={45}
+        />
+      </SponsorContainer>
     </Article>
   );
 }
