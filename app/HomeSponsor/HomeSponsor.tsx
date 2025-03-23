3da0f93c9d1d7fe2d200155e1504519da458b859
Riley Ho <riley@rileychh.dev>
2025-03-24 01:54:00 +0800

    feat: add new GDG on Campus images and update imports to WebP format

--- app/HomeSponsor/HomeSponsor.tsx
+++ app/HomeSponsor/HomeSponsor.tsx
@@ -3,8 +3,9 @@ import Article from "../components/Article";
 import H1 from "../components/H1";
 import H2 from "../components/H2";
 import SponsorContainer from "./SponsorContainer";
-import image_nccu from "./assets/gdg_on_campus_nccu.svg";
-import image_nthu from "./assets/gdg_on_campus_nthu.svg";
+import image_nccu from "./assets/gdg_on_campus_nccu.webp";
+import image_nthu from "./assets/gdg_on_campus_nthu.webp";
+import image_ntust from "./assets/gdg_on_campus_ntust.webp";
 import image_tmu_ieec from "./assets/tmu_ieec.svg";
 
 export default function HomeSponsor() {
@@ -14,24 +15,9 @@ export default function HomeSponsor() {
 
       <H2>主辦單位</H2>
       <SponsorContainer>
-        <Image
-          src={image_nccu.src}
-          alt="GDG on Campus NCCU"
-          width={180}
-          height={30}
-        />
-        <Image
-          src={image_nthu.src}
-          alt="GDG on Campus NTHU"
-          width={180}
-          height={30}
-        />
-        <Image
-          src={image_nthu.src}
-          alt="GDG on Campus NTUST"
-          width={180}
-          height={30}
-        />
+        <Image src={image_nccu} alt="GDG on Campus NCCU" height={40} />
+        <Image src={image_nthu} alt="GDG on Campus NTHU" height={40} />
+        <Image src={image_ntust} alt="GDG on Campus NTUST" height={40} />
       </SponsorContainer>
 
       <H2>協辦單位</H2>
@@ -39,12 +25,7 @@ export default function HomeSponsor() {
         <p className="text-sm/relaxed tracking-wide">
           全台 GDG On Campus（共26間學校）
         </p>
-        <Image
-          src={image_tmu_ieec.src}
-          alt="北醫創新創業教育中心"
-          width={184}
-          height={45}
-        />
+        <Image src={image_tmu_ieec} alt="北醫創新創業教育中心" height={40} />
       </SponsorContainer>
     </Article>
   );
