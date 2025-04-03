e5fdfad60377ce9da830f63f4861491d1de35163
Yi-Jyun Pan <me@pan93.com>
2025-04-03 19:55:46 +0800

    refactor: use <picture> element for responsive image handling in EventArrangement

--- app/HomeInformation/EventArrangement.tsx
+++ app/HomeInformation/EventArrangement.tsx
@@ -4,12 +4,21 @@ import event_arrangement_mobile from "./event_arrangement_mobile.svg";
 export default function EventArrangement() {
   return (
     <div className="w-full h-auto mb-5">
-      <div className="hidden md:block">
-        <img src={event_arrangement_desktop.src} alt={"Event Arrangement"} />
-      </div>
-      <div className="block md:hidden">
-        <img src={event_arrangement_mobile.src} alt={"Event Arrangement"} />
-      </div>
+      <picture>
+        <source
+          media="(min-width: 768px)"
+          srcSet={event_arrangement_desktop.src}
+        />
+        <source
+          media="(max-width: 767px)"
+          srcSet={event_arrangement_mobile.src}
+        />
+        <img
+          src={event_arrangement_mobile.src}
+          alt="Event Arrangement"
+          className="w-full h-auto"
+        />
+      </picture>
     </div>
   );
 }
